83181939ebcf1a6171e4d31792d2e709863a26aa
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-27 21:52:23 -0300

    finalizado

--- 04_even_or_odd/main.py
+++ 04_even_or_odd/main.py
@@ -3,5 +3,12 @@
 def even_or_odd():
     try:
         number = int(input('Digite um número inteiro: '))
+        if number % 2 == 0:
+            return print(f'{number} é par')
+        else:
+            return print(f'{number} é ímpar')
+            
     except ValueError as error:
         return print('Valor não é um número inteiro')
+    
+even_or_odd()
