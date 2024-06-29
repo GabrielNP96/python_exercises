19ba3b821b9c38fc8d58df75445e9dfde0fb72c8
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-29 07:19:50 -0300

    função para pegar dados do usuario 50%

---
+++ 08_Celsius_para_Fahrenheit/main.py
@@ -0,0 +1,21 @@
+import os
+#Escreva um programa que converta temperaturas de Celsius para Fahrenheit e vice-versa.
+
+def get_user_number():
+    i = True
+    while i == True:
+        celsius_or_fahrenheit = input("Para converter Celsius para Fahrenheit digite 1.\nPara coverter Fahrenheit para Celsius Digite 2.\nDigite aqui: ")
+        try:
+            if int(celsius_or_fahrenheit) == 1:
+                    print('ok')
+                    break
+            elif int(celsius_or_fahrenheit) == 2:
+                    print('ok')
+                    break
+            else: 
+                print('Digite apenas 1 ou 2')
+                print('-' * 15)
+        except ValueError:
+            print('Digite apenas 1 ou 2')
+            print('-' * 15)
+get_user_number()
