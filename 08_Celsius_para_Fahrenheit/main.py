a8d996709d2ab610e53a82fcff042d0ef2e2167d
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-30 16:22:30 -0300

    finalizado

--- 08_Celsius_para_Fahrenheit/main.py
+++ 08_Celsius_para_Fahrenheit/main.py
@@ -2,10 +2,13 @@ import os
 #Escreva um programa que converta temperaturas de Celsius para Fahrenheit e vice-versa.
 
 def celsius_to_fahrenheit(number):
-     return f'{number}° é igual á {(number * 9/5) + 32}'
+      fahrenheit =  (number * 9/5) + 32   
+      return f'{number}° é igual á {fahrenheit:.2f}'
+          
 
 def fahrenheit_to_celciusus(number):
-     return f'{number}° é igual á {(number - 32) * 5/9}'
+     celsius = (number - 32) * 5/9
+     return f'{number}° é igual á {celsius:.2f}'
 
 def conversor():
     i = True
@@ -13,15 +16,20 @@ def conversor():
         celsius_or_fahrenheit = input("Para converter Celsius para Fahrenheit digite 1.\nPara coverter Fahrenheit para Celsius Digite 2.\nDigite aqui: ")
         try:
             if int(celsius_or_fahrenheit) == 1:
-                    print('ok')
+                    Celsius = float(input('Digite a quantidade á ser convertida: '))
+                    print(celsius_to_fahrenheit(Celsius))
                     break
             elif int(celsius_or_fahrenheit) == 2:
-                    print('ok')
+                    fahrenheit = float(input('Digite a quantidade á ser convertida: '))
+                    print(fahrenheit_to_celciusus(fahrenheit))
                     break
             else: 
                 print('Digite apenas 1 ou 2')
                 print('-' * 15)
         except ValueError:
-            print('Digite apenas 1 ou 2')
-            print('-' * 15)
+            if celsius_or_fahrenheit != 1 or celsius_or_fahrenheit != 2: 
+                print('Digite apenas 1 ou 2')
+                print('-' * 15)
+            if Celsius != float or fahrenheit != float:
+                 print('Digite apenas números')
 conversor()
