20fb61f8fb421447001fc1788020b8bcac89fdf2
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-01 14:03:04 -0300

    finalizado

--- 10_desconto_simples/main.py
+++ 10_desconto_simples/main.py
@@ -4,14 +4,22 @@
 '''
 
 def get_user_data():
-    print('Bem vindo ao programa de descontos')
+    print(f'Bem vindo ao programa de descontos\n{'-' * 25}')
 
     i = True
     while i == True:
         try:
-            price_of_the_product = float(input('Digite o preço do seu produto'))
+            price_of_the_product = float(input('Digite o preço do seu produto: '))
             discount_amount = float(input('Agora digite o valor do desconto: '))
-            if price_of_the_product < 0.01 and discount_amount < 0.01:
-                ...
+            if price_of_the_product >= 0.01 and discount_amount >= 0.01:
+                return (price_of_the_product, discount_amount)
+            print('Digite valores válidos')
         except ValueError:
             print('Digite valores válidos')
+
+def simple_discount(price, discount):
+    full_discount = price - ((price / 100) * discount)
+    return f'Sua compra vai ser de {price}R$ com {discount}% de desconto por {full_discount:.2f}R$ '
+
+user_data = get_user_data()
+print(simple_discount(user_data[0], user_data[1]))
