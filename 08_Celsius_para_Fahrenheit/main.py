71f188462537a769cf54d353ac9d7ebb86360445
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-01 06:15:48 -0300

    finalizada

--- 08_Celsius_para_Fahrenheit/main.py
+++ 08_Celsius_para_Fahrenheit/main.py
@@ -1,13 +1,19 @@
 #Escreva um programa que converta temperaturas de Celsius para Fahrenheit e vice-versa.
 
 def celsius_to_fahrenheit(number):
-      fahrenheit =  (number * 9/5) + 32   
-      return f'{number}° é igual á {fahrenheit:.2f}'
+      try:
+        fahrenheit =  (float(number) * 9/5) + 32   
+        return f'{number}° é igual á {fahrenheit:.2f}°'
+      except ValueError:
+          return print('Você não digitou um número')
           
 
 def fahrenheit_to_celciusus(number):
-     celsius = (number - 32) * 5/9
-     return f'{number}° é igual á {celsius:.2f}'
+     try:
+        celsius = (float(number) - 32) * 5/9
+        return f'{number}° é igual á {celsius:.2f}°'
+     except ValueError:
+         return print('Você não digitou um número')
 
 def conversor():
     i = True
@@ -15,11 +21,11 @@ def conversor():
         celsius_or_fahrenheit = input("Para converter Celsius para Fahrenheit digite 1.\nPara coverter Fahrenheit para Celsius Digite 2.\nDigite aqui: ")
         try:
             if int(celsius_or_fahrenheit) == 1:
-                    Celsius = float(input('Digite a quantidade á ser convertida: '))
-                    print(celsius_to_fahrenheit(Celsius))
+                    celsius =input('Digite a quantidade á ser convertida: ')
+                    print(celsius_to_fahrenheit(celsius))
                     break
             elif int(celsius_or_fahrenheit) == 2:
-                    fahrenheit = float(input('Digite a quantidade á ser convertida: '))
+                    fahrenheit =input('Digite a quantidade á ser convertida: ')
                     print(fahrenheit_to_celciusus(fahrenheit))
                     break
             else: 
@@ -29,6 +35,4 @@ def conversor():
             if celsius_or_fahrenheit != 1 or celsius_or_fahrenheit != 2: 
                 print('Digite apenas 1 ou 2')
                 print('-' * 15)
-            if Celsius != float or fahrenheit != float:
-                 print('Digite apenas números')
 conversor()
